83872d32fbfa34ca570d5b1aaf2a396b6a7af09c
albyp <alby.palmer@hotmail.com>
2024-08-29 18:24:39 +0800

    Init class-33 index.js

---
+++ class-33/index.js
@@ -0,0 +1,12 @@
+// for loop
+
+// array
+let fruits = ['apple', 'banana', 'pineapple', 'orange', 'grape'];
+
+for(let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+    // output = apple banana pineapple orange grape
+}
+
+
+// while loop
