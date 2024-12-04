4bb16bfb9825df2d3b6dd379e8dd5edf7ae67939
albyp <alby.palmer@hotmail.com>
2024-12-04 09:21:07 +0800

    server initiating, env and routes

---
+++ back-end-project/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+// Route pointers
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
+
+// Load .env config
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// Default route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+});
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+// Routes
+app.use('/auth', authRoutes);
+app.user('/tasks', taskRoutes);
+
+// Define port and start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// MongoDB Error Logging
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
